cda32ecb76d271f7f7b09c49137d4bc6c0e6ec86
pgtrader1975 <gandhip@live.com.au>
2017-02-21 06:37:31 +1100

    Update data-preparation.py
    Add file name into read_csv function

--- data-preparation.py
+++ data-preparation.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 #import dataset
-dataset = pd.read_csv()
+dataset = pd.read_csv('Data.csv')
 x = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 3].values
 
