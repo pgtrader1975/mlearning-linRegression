660be2eeef2cbb378ae9254f4b12d287a2b4ae91
pgtrader1975 <gandhip@live.com.au>
2017-02-22 05:27:28 +1100

    Update data-preparation.r

--- data-preparation.r
+++ data-preparation.r
@@ -1,1 +1,6 @@
+#data preprocessing
+data <- read.csv("C:/Users/parag/Udemy/A_Z_DataScience_MachineLearning/Data.csv")
 
+#missing data
+data$Age = ifelse(is.na(dataset$Age),
+                  ave(data$Age, FUN = function(m)))
